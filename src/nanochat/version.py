8b49ba37b37cb61b9bee59952b0829b48fb3926e
thejudge22 <44528762+thejudge22@users.noreply.github.com>
2026-01-22 13:30:01 -0600

    chore: finalize changes for v0.7.0 release

--- src/nanochat/version.py
+++ src/nanochat/version.py
@@ -1,6 +1,6 @@
 """Version and application metadata for NanoChat Desktop."""
 
-__version__ = "0.6.0"
+__version__ = "0.7.0"
 APP_NAME = "NanoChat Desktop"
 APP_ID = "com.nanogpt.NanoChat"
 GITHUB_URL = "https://github.com/nanogpt-community/nanochat-desktop"
